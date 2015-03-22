3db41e1266811b20c3f52f749265f6c9d416c1bb
Shoghi Cervantes <shoghicp@gmail.com>
2015-03-22 22:56:06 +0100

    Updated ThreadedFactory

--- ThreadedFactory.php
+++ ThreadedFactory.php
@@ -15,26 +15,63 @@
  * GNU General Public License for more details.
 */
 
-abstract class ThreadedFactory{
-	protected static $threadedList = [];
-	
+class ThreadedFactory{
+	/** @var ThreadedFactory */
+	protected static $instance;
+	/** @var \Threaded[] */
+	protected $threadedList = [];
+
+	protected function __construct(){
+		global $threadedFactoryInstance;
+		$threadedFactoryInstance = $this;
+		self::$instance = $this;
+	}
+
 	/**
-     * @return \Threaded
-     */
-	public static function create(){
-		$threaded = new \Threaded;
-		self::$threadedList[] = $threaded;
+	 * @return ThreadedFactory
+	 */
+	public static function getInstance(){
+		if(self::$instance === null){
+			global $threadedFactoryInstance;
+			if($threadedFactoryInstance instanceof ThreadedFactory){
+				self::$instance = $threadedFactoryInstance;
+			}else{
+				new ThreadedFactory();
+			}
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * @param \Threaded $class
+	 * @param ...$arguments
+	 *
+	 * @return \Threaded
+	 */
+	public static function create($class = \Threaded::class, ...$arguments){
+		/** @var \Threaded $threaded */
+		$threaded = new $class(...$arguments);
+		self::getInstance()->threadedList[spl_object_hash($threaded)] = $threaded;
 		return $threaded;
 	}
 	
 	public static function destroy(\Threaded $threaded){
-		foreach(self::$threadedList as $i => $t){
-			if($t === $threaded){
-				unset(self::$threadedList[$i]);
-				return true;
-			}
+		$instance = self::getInstance();
+		if(isset($instance->threadedList[$hash = spl_object_hash($threaded)])){
+			$threaded->synchronized(function(\Threaded $t){
+				$t->notify();
+			}, $threaded);
+			unset($instance->threadedList[$hash]);
+			return true;
 		}
-		
 		return false;
+	}
+
+	/**
+	 * @return \Threaded[]
+	 */
+	public static function all(){
+		return self::getInstance()->threadedList;
 	}
 }
