c4ab2d584cdb26f491959e54a32b6598bbbb8d73
Dylan K. Taylor <odigiman@gmail.com>
2018-10-06 15:21:33 +0100

    Debloat ClassLoader interface this interface should present the bare essentials needed to create a class loader. Instead, it's pretty much a carbon copy of BaseClassLoader's API.
    I want to implement a threaded ComposerClassLoader which implements this interface, which is impossible with this bullshit going on.

--- ClassLoader.php
+++ ClassLoader.php
@@ -18,38 +18,12 @@
 interface ClassLoader{
 
 	/**
-	 * @param ClassLoader $parent
-	 */
-	public function __construct(ClassLoader $parent = null);
-
-	/**
 	 * Adds a path to the lookup list
 	 *
 	 * @param string $path
 	 * @param bool   $prepend
 	 */
 	public function addPath($path, $prepend = false);
-
-	/**
-	 * Removes a path from the lookup list
-	 *
-	 * @param $path
-	 */
-	public function removePath($path);
-
-	/**
-	 * Returns an array of the classes loaded
-	 *
-	 * @return string[]
-	 */
-	public function getClasses();
-
-	/**
-	 * Returns the parent ClassLoader, if any
-	 *
-	 * @return ClassLoader
-	 */
-	public function getParent();
 
 	/**
 	 * Attaches the ClassLoader to the PHP runtime
@@ -59,22 +33,4 @@ interface ClassLoader{
 	 * @return bool
 	 */
 	public function register($prepend = false);
-
-	/**
-	 * Called when there is a class to load
-	 *
-	 * @param string $name
-	 *
-	 * @return bool
-	 */
-	public function loadClass($name);
-
-	/**
-	 * Returns the path for the class, if any
-	 *
-	 * @param string $name
-	 *
-	 * @return string|null
-	 */
-	public function findClass($name);
 }
