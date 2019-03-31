463152d5d2ee052a89fe44dcc27b56663f891912
Dylan K. Taylor <odigiman@gmail.com>
2019-03-31 17:30:20 +0100

    Added UnsupportedOperationException

---
+++ UnsupportedOperationException.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * PocketMine Standard PHP Library
+ * Copyright (C) 2019 PMMP Team <https://github.com/pmmp/SPL>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+*/
+
+declare(strict_types=1);
+
+class UnsupportedOperationException extends \LogicException{
+
+}
