0a81f1e424035a604c2e4d7effb3ab855b45505b
Dylan K. Taylor <odigiman@gmail.com>
2019-08-25 19:29:14 +0100

    BaseClassLoader: fix race condition in path prepending

--- BaseClassLoader.php
+++ BaseClassLoader.php
@@ -51,7 +51,7 @@ class BaseClassLoader extends \Threaded implements ClassLoader{
 		}
 
 		if($prepend){
-			$this->synchronized(function($path){
+			$this->lookup->synchronized(function($path){
 				$entries = $this->getAndRemoveLookupEntries();
 				$this->lookup[] = $path;
 				foreach($entries as $entry){
